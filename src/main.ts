04496565a6dd4a1259e5c12ae98277e9534b96b3
Bancarel Gatien <gatien.bancarel@edu.ece.fr>
2019-12-15 21:27:40 +0100

    recuperation text fichier signin

--- src/main.ts
+++ src/main.ts
@@ -1,21 +1,27 @@
+//import { emptyField } from './blindage/emptyField'
+
 let express = require("express")
+let bodyParser = require('body-parser')
+let app = express();
 
-const app = express();
-
-const PORT = 8080;
+//Middleware
 
 app.use(express.static('public'))
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
 
-app.listen(PORT)
+//Moteur de template
 
 app.set('view engine', 'ejs')
+
+//Routes get
 
 app.get('/', (req: any, res: any) => {
     res.render('pages/home', { test: 'fc' })
 })
 
 app.get('/signIn', (req : any,res : any) => {
-	res.render('pages/signIn',{test: 'fc'})
+    res.render('pages/signIn',{test: 'fc'})
 })
 
 app.get('/signUp', (req : any,res : any) => {
@@ -25,4 +31,17 @@ app.get('/signUp', (req : any,res : any) => {
 app.get('/logged', (req: any,res :any) => {
 	res.render('pages/logged',{test: 'fc'})
 })
+
+//Routes post
+
+app.post('/signIn', (req : any,res : any) => {
+    if (req.body.email === undefined || req.body.email ===  "" || req.body.password === undefined || req.body.password ===  "") {
+        res.render('pages/signIn',{error: "You need to write something"})
+    }
+    res.redirect('/logged')
+    console.log(req.body.email)
+})
+
+const PORT = 8080;
+app.listen(PORT)
 
