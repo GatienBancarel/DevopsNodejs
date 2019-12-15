201f253408f144844b88d54b93c98021fa21c233
Bancarel Gatien <gatien.bancarel@edu.ece.fr>
2019-12-15 18:00:03 +0100

    modifications noms boutons

--- src/main.ts
+++ src/main.ts
@@ -4,11 +4,11 @@ const app = express();
 
 const PORT = 8080;
 
+app.use(express.static('public'))
+
 app.listen(PORT)
 
 app.set('view engine', 'ejs')
-
-app.use(express.static('public'))
 
 app.get('/', (req: any, res: any) => {
     res.render('pages/home', { test: 'fc' })
